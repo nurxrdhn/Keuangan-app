51813e3626e4d96a6c2a782df430b832b77215c4
nurxrdhn <ramadhannur148@gmail.com>
2025-11-17 14:24:37 +0700

    Set simple welcome page + Google verification

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,8 +2,6 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <title>Aplikasi Keuangan Sederhana</title>
 
     <!-- Google Search Console verification -->
@@ -12,23 +10,9 @@
     <meta name="description" content="Aplikasi web untuk mencatat pemasukan, pengeluaran, dan menghitung saldo otomatis.">
     <meta name="keywords" content="aplikasi keuangan, catatan keuangan, pemasukan pengeluaran, aplikasi web keuangan">
     <meta name="author" content="nurxrdhn">
-
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
-
-    <!-- Styles / Scripts -->
-    @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    @else
-        <style>
-            /* backup tailwind inline di sini */
-        </style>
-    @endif
 </head>
-
 <body>
-    <h1>Welcome</h1>
-    <p>Aplikasi Keuangan Sederhana</p>
+    <h1>Halo, ini Aplikasi Keuangan Sederhana</h1>
+    <p>Catatan pemasukan, pengeluaran, dan saldo otomatis.</p>
 </body>
 </html>
