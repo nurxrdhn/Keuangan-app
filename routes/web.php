3812f604aa56c50d153c178e3d6b70fa98c0594e
nurxrdhn <ramadhannur148@gmail.com>
2025-11-18 01:16:00 +0700

    Add sitemap.xml route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/sitemap.xml', function () {
+    return response()->file(public_path('sitemap.xml'), [
+        'Content-Type' => 'application/xml'
+    ]);
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
