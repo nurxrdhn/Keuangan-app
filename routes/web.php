3f12250d695a6f534ff5568ac8281d989d71a810
nurxrdhn <ramadhannur148@gmail.com>
2025-11-17 12:03:23 +0700

    Fix Google verification file + cleanup route

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('googleba37d536d4ece2d3.html', function () {
-    return response('google-site-verification: googleba37d536d4ece2d3.html', 200)
-        ->header('Content-Type', 'text/plain');
-});
-
 Route::get('/', function () {
     return view('welcome');
 });
