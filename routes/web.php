7474d5afe12973709bf8938bcea8b938c8ea435d
nurxrdhn <ramadhannur148@gmail.com>
2025-11-17 08:02:59 +0700

    Add Google verification route

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/googleba37d536d4ece2d3.html', function () {
+Route::get('googleba37d536d4ece2d3.html', function () {
     return response('google-site-verification: googleba37d536d4ece2d3.html', 200)
         ->header('Content-Type', 'text/plain');
 });
